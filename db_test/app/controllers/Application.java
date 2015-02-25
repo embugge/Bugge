d38ee342e2527351e1b843e731cb06efb47555ca
Erik Bugge <embugge@hotmail.com>
2015-02-25 01:22:15 +0100

    Play test

---
+++ db_test/app/controllers/Application.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+
+    public static Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result statistics() {
+        return ok(statistics.render("Your new application is ready."));
+    }
+
+}
